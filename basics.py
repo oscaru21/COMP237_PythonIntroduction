b14ecf4111b2cb0f1435490802ec2a4fa4a09f14
Oscar Umaña <oscarumana1021@gmail.com>
2022-09-09 13:43:39 -0400

    first commit

---
+++ basics.py
@@ -0,0 +1,42 @@
+#Arithmetic operations
+addition = 2 + 2
+substraction = 2 - 2
+division = 2 / 2
+multiplication = 2 * 2
+module = 2 % 2
+
+print("addition result = %s" % (addition))
+print("substraction result = %s" % (substraction))
+print("division result = %s" % (division))
+print("multiplication result = %s" % (multiplication))
+print("module result = %s" % (module))
+
+#Relationals operations TRUE/FALSE
+
+greater_than = 2 > 2
+greater_equal_than = 2 >= 2
+less_than = 2 < 2
+less_equal_than = 2 <= 2
+equal = 2 == 2
+different = 2 != 2
+
+print("greater than result = %s" % (greater_than))
+print("greater or equal than result = %s" % (greater_equal_than))
+print("less result = %s" % (less_than))
+print("less or equal than result = %s" % (less_equal_than))
+print("equal result = %s" % (equal))
+print("different result = %s" % (different))
+
+#Logical operators
+
+#and operator: both operands are true
+print(True and True)
+print(True and False)
+
+#or operator: at least one operand is true
+print(True or False)
+print(False or False)
+
+#not operator: opposite operand
+print(not False)
+print(not True)
---
+++ conditional_statements.py
@@ -0,0 +1,20 @@
+num1 = 20
+num2 = -20
+
+#if statement
+if num1 != num2:
+    print('numbers are not equal!')
+
+#if-else statement
+if num1 != num2:
+    print('numbers are not equal!')
+else:
+    print('Numbers are equal!')
+
+#if-elif statement
+if num1 > 0 and num2 > 0:
+    print('Positive numbers!')
+elif num1 < 0 and num2 < 0:
+    print('Negative numbers!')
+else:
+    print('Positive and negative numbers!')
